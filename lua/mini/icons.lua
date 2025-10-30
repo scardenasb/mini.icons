4053416337f13d1e1d88fa8c23bee2c0cab2250d
scardenasb <84429399+scardenasb@users.noreply.github.com>
2025-10-29 23:45:46 -0300

    update

--- lua/mini/icons.lua
+++ lua/mini/icons.lua
@@ -1235,6 +1235,7 @@ H.filetype_icons = {
   elmfilt                = { glyph = '󰫲', hl = 'MiniIconsBlue'   },
   elsa                   = { glyph = '󰘧', hl = 'MiniIconsGreen'  },
   elvish                 = { glyph = '', hl = 'MiniIconsGreen'  },
+  env                    = { glyph = '', hl = 'MiniIconsYellow'  },
   epuppet                = { glyph = '', hl = 'MiniIconsYellow' },
   erlang                 = { glyph = '', hl = 'MiniIconsRed'    },
   eruby                  = { glyph = '󰴭', hl = 'MiniIconsOrange' },
